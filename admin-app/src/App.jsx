2d96137a1077eb3b2ced6726ba92ea8676ecc80e
Ramón <ramonchuwii@gmail.com>
2021-11-03 16:00:56 +0100

    probando navbar

--- admin-app/src/App.jsx
+++ admin-app/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Header  from './components/Header';
 
 function App() {
   return (
-    <div className="container">
-      prueba
+    <div className="container ">
+      <Header />
     </div>
   );
 }
---
+++ admin-app/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">RJB App - Admin</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="collapsibleNavbar">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Eventos</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Plantas y Lugares</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">Plantas</a></li>
+                                    <li><a className="dropdown-item" href="#">Puntos de Interés</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Itinerarios</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Header

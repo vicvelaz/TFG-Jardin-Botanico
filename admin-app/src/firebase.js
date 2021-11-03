d872c63bdc62b8a33360a1c2801134ae1b3cab6c
Ramón <ramonchuwii@gmail.com>
2021-11-03 15:20:00 +0100

    fichero config firestore

---
+++ admin-app/src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDIhyA7CsMiytlWwIpU7zD64jMONUPahvg",
+    authDomain: "realjardinbotanicoapp.firebaseapp.com",
+    projectId: "realjardinbotanicoapp",
+    storageBucket: "realjardinbotanicoapp.appspot.com",
+    messagingSenderId: "865397111946",
+    appId: "1:865397111946:web:389a059a73976c02090173",
+    measurementId: "G-KKJJK6H0T0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export {firebase}

3ae9c5e4d690646c688d18c21578def8eea8859e
Ramón <ramonchuwii@gmail.com>
2021-11-09 00:36:58 +0100

    errores className

--- admin-app/src/components/Events.jsx
+++ admin-app/src/components/Events.jsx
@@ -9,39 +9,39 @@ const Events = () => {
             <div className="container d-flex flex-column">
                 <div className="d-flex mt-5">
                     <button className="btn btn-success ms-5" data-bs-toggle="modal" data-bs-target="#nuevoeventomodal">Nuevo Evento</button>
-                    <div class="modal fade text-dark" id="nuevoeventomodal">
-                        <div class="modal-dialog modal-dialog-centered modal-lg">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h4 class="modal-title">Nuevo Evento</h4>
+                    <div className="modal fade text-dark" id="nuevoeventomodal">
+                        <div className="modal-dialog modal-dialog-centered modal-lg">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h4 className="modal-title">Nuevo Evento</h4>
                                     <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
                                 </div>
                                 <form>
-                                    <div class="modal-body">
+                                    <div className="modal-body">
                                         <div className="form-floating mt-3">
-                                            <input type="text" className="form-control" id="name" placeholder="Nombre" name="name" maxlength="50"></input>
-                                            <label for="name">Nombre</label>
+                                            <input type="text" className="form-control" id="name" placeholder="Nombre" name="name" maxLength="50"></input>
+                                            <label htmlFor="name">Nombre</label>
                                         </div>
-                                        <div class="form-floating mt-3">
-                                            <textarea className="form-control" id="description" name="description" placeholder="Descripción" maxlength="200"></textarea>
-                                            <label for="description">Descripción</label>
+                                        <div className="form-floating mt-3">
+                                            <textarea className="form-control" id="description" name="description" placeholder="Descripción" maxLength="200"></textarea>
+                                            <label htmlFor="description">Descripción</label>
                                         </div>
                                         <div className="d-flex mt-4">
                                             <div className="">
-                                                <label className="ms-5 me-2" for="startevent">Inicio evento: </label>
+                                                <label className="ms-5 me-2" htmlFor="startevent">Inicio evento: </label>
                                                 <input type="date" id="startevent" name="startevent"></input>
                                             </div>
                                             <div className="ms-auto me-5">
-                                                <label className="me-2" for="endevent">Fin evento: </label>
+                                                <label className="me-2" htmlFor="endevent">Fin evento: </label>
                                                 <input type="date" id="endevent" name="endevent"></input>
                                             </div>
                                         </div>
                                         <div className="d-flex justify-content-center align-items-center mt-4">
-                                            <label for="formFile" className="form-label">Imagen: </label>
-                                            <input className="form-control w-50 ms-2" type="file" id="formFile"></input>
+                                            <label htmlFor="formFile" className="form-label">Imagen: </label>
+                                            <input className="form-control w-50 ms-2" type="file" accept="image/*" id="formFile"></input>
                                         </div>
                                     </div>
-                                    <div class="modal-footer">
+                                    <div className="modal-footer">
                                         <input type="submit" className="btn btn-success" value="Añadir"></input>
                                         <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
                                     </div>
@@ -49,12 +49,12 @@ const Events = () => {
                             </div>
                         </div>
                     </div>
-                    <div class=" ms-auto me-5">
+                    <div className=" ms-auto me-5">
                         <input type="text" className="form-control form-control-md text-dark" placeholder="Buscar"></input>
                     </div>
                 </div>
                 <div className="mt-4 contenedor rounded">
-                    <table class="table table-striped table-hover">
+                    <table className="table table-striped table-hover">
                         <thead className="table-dark">
                             <tr>
                                 <th>Nombre</th>

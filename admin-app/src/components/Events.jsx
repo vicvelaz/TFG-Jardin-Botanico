ea34875811ed1500f039af8043902d0a0e317156
Ramón <ramonchuwii@gmail.com>
2021-11-08 17:51:29 +0100

    color btn

--- admin-app/src/components/Events.jsx
+++ admin-app/src/components/Events.jsx
@@ -46,14 +46,14 @@ const Events = () => {
                                 <td>El Real Jardín Botánico (RJB) del Consejo Superior de Investigaciones Científicas (CSIC) se suma una edición más a la gran fiesta de la ciencia y la tecnología que supone la celebración de la denomina...</td>
                                 <td>01/11/21</td>
                                 <td><div className="d-flex align-items-center align-self-center">14/11/21</div></td>
-                                <td><div className="d-flex"><button className="btn btn-secondary">Editar</button><button className="btn btn-danger ms-3">Eliminar</button></div></td>
+                                <td><div className="d-flex"><button className="btn btn-success">Editar</button><button className="btn btn-danger ms-3">Eliminar</button></div></td>
                             </tr>
                             <tr>
                                 <td>Naturaleza Encencida</td>
                                 <td>Descubre una nueva edición de la exitosa Naturaleza Encendida, EXPLORIUM, una experiencia inmersiva y llena de sorpresas que te hará viajar a las profundidades en un viaje sensorial completamente dife...</td>
                                 <td>02/11/21</td>
                                 <td>16/01/22</td>
-                                <td><div className="d-flex"><button className="btn btn-secondary">Editar</button><button className="btn btn-danger ms-3">Eliminar</button></div></td>
+                                <td><div className="d-flex"><button className="btn btn-success">Editar</button><button className="btn btn-danger ms-3">Eliminar</button></div></td>
                             </tr>
                         </tbody>
                     </table>

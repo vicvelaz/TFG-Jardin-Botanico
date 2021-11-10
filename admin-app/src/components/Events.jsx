3bd0f9bb393a51f5c6ba7f0533d4671af28e4ffc
Ramón <ramonchuwii@gmail.com>
2021-11-10 21:36:55 +0100

    añadir evento a medias

--- admin-app/src/components/Events.jsx
+++ admin-app/src/components/Events.jsx
@@ -7,6 +7,11 @@ import 'moment/locale/es'
 const Events = () => {
 
     const [eventos,setEventos] = React.useState([]);
+    const [name, setName] = React.useState("");
+    const [description, setDescription] = React.useState("");
+    const [startDate, setStartDate] = React.useState("");
+    const [endDate, setEndDate] = React.useState("");
+    const [image, setImage] = React.useState("");
 
     React.useEffect(()=>{
         const obtenerEventos = async () => {
@@ -26,6 +31,39 @@ const Events = () => {
 
     },[]);
 
+    const nuevoEvento = async(e) => {
+        e.preventDefault();
+        if(!name.trim()||!startDate.trim()||!endDate.trim()){
+            console.log("Los campos están vacios")
+            return
+        }
+        try {
+
+            const nuevoEvento = {
+                name: name,
+                description: description,
+                start_date: new Date(startDate),
+                end_date: new Date(endDate),
+                image: image
+            }
+
+            const data = await db.collection('events').add(nuevoEvento);
+
+            setName('');
+            setDescription('');
+            setStartDate('');
+            setEndDate('');
+            setImage('');
+            setEventos([
+                ...eventos,{...nuevoEvento, id: data.id}
+            ])
+            document.getElementById("formularioeventos").reset();
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className="background">
             <div className="d-flex justify-content-center mt-5">
@@ -41,33 +79,33 @@ const Events = () => {
                                     <h4 className="modal-title">Nuevo Evento</h4>
                                     <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
                                 </div>
-                                <form>
+                                <form id="formularioeventos" onSubmit={nuevoEvento}>
                                     <div className="modal-body">
                                         <div className="form-floating mt-3">
-                                            <input type="text" className="form-control" id="name" placeholder="Nombre" name="name" maxLength="50"></input>
+                                            <input type="text" className="form-control" id="name" placeholder="Nombre" name="name" maxLength="50" onChange={e => setName(e.target.value)}></input>
                                             <label htmlFor="name">Nombre</label>
                                         </div>
                                         <div className="form-floating mt-3">
-                                            <textarea className="form-control" id="description" name="description" placeholder="Descripción" maxLength="200"></textarea>
+                                            <textarea className="form-control" id="description" name="description" placeholder="Descripción" maxLength="200" onChange={e => setDescription(e.target.value)}></textarea>
                                             <label htmlFor="description">Descripción</label>
                                         </div>
                                         <div className="d-flex mt-4">
                                             <div className="">
                                                 <label className="ms-5 me-2" htmlFor="startevent">Inicio evento: </label>
-                                                <input type="date" id="startevent" name="startevent"></input>
+                                                <input type="date" id="startevent" name="startevent" onChange={e => setStartDate(e.target.value)}></input>
                                             </div>
                                             <div className="ms-auto me-5">
                                                 <label className="me-2" htmlFor="endevent">Fin evento: </label>
-                                                <input type="date" id="endevent" name="endevent"></input>
+                                                <input type="date" id="endevent" name="endevent" onChange={e => setEndDate(e.target.value)}></input>
                                             </div>
                                         </div>
                                         <div className="d-flex justify-content-center align-items-center mt-4">
                                             <label htmlFor="formFile" className="form-label">Imagen: </label>
-                                            <input className="form-control w-50 ms-2" type="file" accept="image/*" id="formFile"></input>
+                                            <input className="form-control w-50 ms-2" type="file" accept="image/*" id="formFile" onChange={e => setImage(e.target.value)}></input>
                                         </div>
                                     </div>
                                     <div className="modal-footer">
-                                        <input type="submit" className="btn btn-success" value="Añadir"></input>
+                                        <input type="submit" className="btn btn-success" data-bs-dismiss="modal" value="Añadir"></input>
                                         <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
                                     </div>
                                 </form>

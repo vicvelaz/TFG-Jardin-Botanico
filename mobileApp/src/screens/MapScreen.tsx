e57ea1c421ebb9ef1960656f67c5241b4e89b97c
Ramón Rosa Pérez <ramonros@ucm.es>
2022-02-24 01:41:14 +0100

    Mapa primera versión y coordenadas con geolocalización

--- mobileApp/src/screens/MapScreen.tsx
+++ mobileApp/src/screens/MapScreen.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import Map from '../components/Map';
+import Geolocation from 'react-native-geolocation-service';
+import { Text, PermissionsAndroid } from 'react-native'
+
 
 export const MapScreen = () => {
+
+    const [userPosition, setUserPosition] = useState<Geolocation.GeoPosition>();
+    const [userPositionLat, setUserPositionLat] = useState<number>();
+    const [userPositionLong, setUserPositionLong] = useState<number>();
+
+    async function requestPermissions(){
+        await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+    }
+
+    useEffect(() => {
+        requestPermissions();
+        Geolocation.getCurrentPosition(
+            (position) => {
+              setUserPosition(position);
+              setUserPositionLat(position.coords.latitude);
+              setUserPositionLong(position.coords.longitude);
+            },
+            (error) => {
+              // See error code charts below.
+              console.log(error.code, error.message);
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+    }, []);
+
+    //Poner <Map></Map> para ver mapa de mapbox
     return (
-        <Text>
-            Mapa
-        </Text>
+        <Text>{userPositionLat},{userPositionLong}</Text>
     )
 }

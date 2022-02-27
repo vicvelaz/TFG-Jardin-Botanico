f49886b6a5f6b848a28f404191bd8a11c7906c1e
Ramón Rosa Pérez <ramonros@ucm.es>
2022-02-27 03:21:51 +0100

    comprobacion dentro del jardin

--- mobileApp/src/components/Map.tsx
+++ mobileApp/src/components/Map.tsx
@@ -1,24 +1,67 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert, PermissionsAndroid } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
+import Geolocation from 'react-native-geolocation-service';
+import {booleanPointInPolygon,point,polygon} from '@turf/turf';
 
 MapboxGL.setAccessToken('pk.eyJ1IjoicmFteG5jaHYiLCJhIjoiY2t6c2IybzZrNXB2aDMwbzFnbmFsOXptNSJ9.CQqoytOo3yM-pGaCRIGgjw');
 
-interface Props {
-  lat: number,
-  lng: number,
-}
+const Map = () => {
 
-const Map = ({ lat,lng }: Props) => {
+    //constantes del jardin con coordenadas fijas
     const centerLng : number = -3.690750;
     const centerLat : number = 40.411147;
+    const perimetro = polygon([[
+      [-3.6925971508026127,40.41231565042066],
+      [-3.6919802427291875,40.40973422067125],
+      [-3.6890941858291626,40.40976689761599],
+      [-3.68891716003418,40.409893520626945],
+      [-3.6890029907226562,40.41200114608072],
+      [-3.6904352903366084,40.41233607272957],
+      [-3.6904406547546387,40.41256888661311],
+      [-3.690462112426758,40.41262198440359],
+      [-3.691293597221374,40.41258930884518],
+      [-3.691293597221374,40.41247085981296],
+      [-3.6925971508026127,40.41231565042066]
+    ]]);
+
+    //estados para la posicion del usuario
+    const [userPositionLat, setUserPositionLat] = React.useState<number>(40.412386);
+    const [userPositionLong, setUserPositionLong] = React.useState<number>(-3.691977);
+
+    React.useEffect(() => {
+        requestPermissions();
+        checkUserPosition();
+    }, []);
+
+    async function requestPermissions(){
+      await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+    }
+
+    const checkUserPosition = () => {
+      Geolocation.getCurrentPosition(
+            (position) => {
+              setUserPositionLat(position.coords.latitude);
+              setUserPositionLong(position.coords.longitude);
+              const userCoords = point([position.coords.longitude,position.coords.latitude]);
+              if(!booleanPointInPolygon(userCoords,perimetro)){
+                Alert.alert("Estás fuera del Jardín Botánico","Por favor, camina hacia el jardín para poder utilizar el mapa");
+              }
+            },
+            (error) => {
+              // See error code charts below.
+              console.log(error.code, error.message);
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+      );
+    }
 
     return (
       <View style={styles.page}>
         <View style={styles.container}>
           <MapboxGL.MapView style={styles.map} styleURL={"mapbox://styles/ramxnchv/cl006l6ye000614mufkp230xm"}>
             <MapboxGL.Camera zoomLevel={16.15} centerCoordinate={[centerLng,centerLat]} />
-            <MapboxGL.UserLocation androidRenderMode='compass' showsUserHeadingIndicator={true} />
+            <MapboxGL.UserLocation androidRenderMode='compass' showsUserHeadingIndicator={true} onUpdate={() => checkUserPosition()}/>
           </MapboxGL.MapView>
         </View>
       </View>
--- mobileApp/src/screens/MapScreen.tsx
+++ mobileApp/src/screens/MapScreen.tsx
@@ -1,38 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Map from '../components/Map';
-import Geolocation from 'react-native-geolocation-service';
-import { Text, PermissionsAndroid } from 'react-native'
-
 
 export const MapScreen = () => {
-    const userposition:number = 2;
-
-    const [userPosition, setUserPosition] = useState<Geolocation.GeoPosition>();
-    const [userPositionLat, setUserPositionLat] = useState<number>(40.412386);
-    const [userPositionLong, setUserPositionLong] = useState<number>(-3.691977);
-
-    async function requestPermissions(){
-        await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
-    }
-
-    useEffect(() => {
-        requestPermissions();
-        Geolocation.getCurrentPosition(
-            (position) => {
-              setUserPosition(position);
-              setUserPositionLat(position.coords.latitude);
-              setUserPositionLong(position.coords.longitude);
-            },
-            (error) => {
-              // See error code charts below.
-              console.log(error.code, error.message);
-            },
-            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-        );
-    }, []);
-
-    //Poner <Text>{userPositionLat},{userPositionLong}</Text> para ver mapa de mapbox
     return (
-        <Map lat={userPositionLat} lng={userPositionLong}></Map>
+        <Map></Map>
     )
 }
